d07c208d154fb3a159bd6c61a868890d8f0b3fda
EDK <kde9604@naver.com>
2020-07-01 22:58:46 +0900

    upDataMovieData

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import MyForm from './components/MyForm';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App: React.FC = () => {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
+function App() {
+  // movieState , LoadingState
+  const [movieData, setData] = useState<object>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  // Arrow Function
+  const getMovies = async () => {
+    const data = await axios.get(
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating',
+    );
+    setData(data.data);
+    setLoading(true);
   };
-  return <MyForm onSubmit={onSubmit} />;
-};
+
+  // Like ComponentDidMount
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoading ? <div> loadingDone </div> : <div> Loading... </div>}
+    </div>
+  );
+}
 
 export default App;
