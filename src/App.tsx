0a837fc322a9a9fdb2f94125495f6b6ef7e5e39b
kim dong eon <kde9604@naver.com>
2020-07-20 22:49:35 +0900

    Set prettier

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,14 @@ function App() {
   const [movieData, setData] = useState<object>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
 
-  // Arrow Function
   const getMovies = async () => {
     const data: object = await axios.get(
       'https://yts-proxy.now.sh/list_movies.json?sort_by=rating',
-      {
-        validateStatus: function (status) {
-          // 상태 코드가 500 이상일 경우 거부. 나머지(500보다 작은)는 허용.
-          return status < 500;
-        },
-      },
     );
     setData(data);
     setLoading(true);
   };
 
-  // Like ComponentDidMount
   useEffect(() => {
     getMovies();
   }, []);
